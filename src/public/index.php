d7e25306697f7fc0f8a996f0a73cdb17b2e2853e
gankun <gankichi1126@gmail.com>
2023-11-02 20:35:20 +0900

    fix: レビュー修正

--- src/public/index.php
+++ src/public/index.php
@@ -1,12 +1,12 @@
 <?php
 $num = 7;
 
-if ($num <= 3) {
-echo "凶";
-} elseif ($num >= 4 && $num <=7) {
-echo "吉";
+if (1 <= $num && $num <= 3) {
+    echo "凶";
+} elseif (4 <= $num && $num <=7) {
+    echo "吉";
 } else {
-echo "大吉";
+    echo "大吉";
 }
 
 ?>
